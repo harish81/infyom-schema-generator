cf9aced5f35d44416decda6be57c63ed92154fdc
AVC <nandoliyaharish@gmail.com>
2019-09-28 17:48:54 +0530

    Add Build for github pages

---
+++ build/precache-manifest.9b6be93889b2a8a87348954e568e34aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd3d88c999300a56d0b77f9af2b0e98",
+    "url": "/infyom-schema-generator/index.html"
+  },
+  {
+    "revision": "24a8e2c8133ff9443e67",
+    "url": "/infyom-schema-generator/static/css/main.de1a1365.chunk.css"
+  },
+  {
+    "revision": "510d833f67f2f676079a",
+    "url": "/infyom-schema-generator/static/js/2.cc691c5b.chunk.js"
+  },
+  {
+    "revision": "24a8e2c8133ff9443e67",
+    "url": "/infyom-schema-generator/static/js/main.2bffef5d.chunk.js"
+  },
+  {
+    "revision": "e513f069162b153fd049",
+    "url": "/infyom-schema-generator/static/js/runtime-main.19c652b2.js"
+  }
+]);
