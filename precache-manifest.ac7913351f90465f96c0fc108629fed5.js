6f47620cce7fe92d65ff0c566a29e947e0aea27c
harish <nandoliyaharish@gmail.com>
2019-10-02 08:46:28 +0530

    Update gh-pages

--- precache-manifest.ac7913351f90465f96c0fc108629fed5.js
+++ precache-manifest.ac7913351f90465f96c0fc108629fed5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5ff0cdfcea3c65fabefc9b3f06d426a8",
+    "revision": "992ccbc37dabe5c9e7a3c3e5adbb3656",
     "url": "/infyom-schema-generator/index.html"
   },
   {
-    "revision": "c5a08c57007b47a7093d",
-    "url": "/infyom-schema-generator/static/css/main.99406395.chunk.css"
+    "revision": "050958230d44ccb72ce1",
+    "url": "/infyom-schema-generator/static/css/main.6cecc2a4.chunk.css"
   },
   {
     "revision": "70d890693325c77fac68",
     "url": "/infyom-schema-generator/static/js/2.633c9cdc.chunk.js"
   },
   {
-    "revision": "c5a08c57007b47a7093d",
-    "url": "/infyom-schema-generator/static/js/main.da3b1d4e.chunk.js"
+    "revision": "050958230d44ccb72ce1",
+    "url": "/infyom-schema-generator/static/js/main.8fde34d0.chunk.js"
   },
   {
     "revision": "e513f069162b153fd049",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/infyom-schema-generator/precache-manifest.b43251be1dfb5fdc3fcaf6a878dd3fc8.js"
+  "/infyom-schema-generator/precache-manifest.ac7913351f90465f96c0fc108629fed5.js"
 );
 
 self.addEventListener('message', (event) => {
